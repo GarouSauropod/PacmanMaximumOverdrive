a48fecec5061b9573f046ce3b8715a582ef5fe97
GarouSauropod <alexandre@sauropodstudio.com>
2019-08-24 23:40:38 -0400

    Added a board manager as well as a level prop class to make level generation fully work

---
+++ Assets/Scripts/LevelDesignProps.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DataUtility;
+using UnityEngine;
+
+namespace LevelDesignProps
+{
+
+    [System.Serializable]
+	public class LevelProp
+    {
+        public string name;
+        public GameObject physicalObject;
+        public Vector3 positionOffset;
+        public IntVector2[] impassableSquares;
+
+        public LevelProp(string _name, GameObject _prop, Vector3 _offset, IntVector2[] _impassableSquares)
+        {
+            name = _name;
+            physicalObject = _prop;
+            positionOffset = _offset;
+            impassableSquares = _impassableSquares;
+        }
+    }
+}
---
+++ Assets/Scripts/Managers/BoardManager.cs
@@ -0,0 +1,73 @@
+﻿using DataUtility;
+using UnityEngine;
+using System;
+using System.Collections.Generic;
+using GridUtilities;
+using LevelDesignProps;
+
+public class BoardManager : MonoBehaviour
+{
+
+    private static BoardManager boardManager;
+
+    public static BoardManager instance
+    {
+        get
+        {
+            if (!boardManager)
+            {
+                boardManager = FindObjectOfType(typeof(BoardManager)) as BoardManager;
+
+                if (!boardManager)
+                {
+                    Debug.LogError("There needs to be one active BoardManager script on a GameObject in your scene.");
+                }
+            }
+
+            return boardManager;
+        }
+    }
+
+    [NonSerialized]
+    public LevelGrid grid = new LevelGrid();
+    [NonSerialized]
+    public int squareSize = 1;
+    private Vector3 gridOrigin = Vector3.zero;
+
+    private List<GameObject> instantiatedLevelPieces = new List<GameObject>();
+
+    void Awake()
+    {
+        PopulateGrid(new Vector3(gridOrigin.x + 1, gridOrigin.y, gridOrigin.z + 1), 41, 23);
+        grid.AddAllNeighbors();
+    }
+
+    public void PopulateGrid(Vector3 _startingPoint, int _width, int _height)
+    {
+        grid.squareArray = new Square[_width, _height];
+
+        for (int i = 0; i < _width; i++)
+        {
+            for (int j = 0; j < _height; j++)
+            {
+                grid.squareArray[i, j] = new Square();
+                grid.squareArray[i, j].position = new Vector3(i*squareSize + _startingPoint.x, 0 + _startingPoint.y, j*squareSize + _startingPoint.z);
+                grid.squareArray[i, j].state = Square.State.Free;
+            }
+        }
+    }
+
+    public void AddPropToGrid(LevelProp _prop, IntVector2 _position)
+    {
+        Square referenceSquare = grid.squareArray[_position.x, _position.y];
+        for (int i = 0; i < _prop.impassableSquares.Length; i++)
+        {
+            IntVector2 impassablePosition = _prop.impassableSquares[i];
+            grid.squareArray[_position.x + impassablePosition.x, _position.y + impassablePosition.y].state = Square.State.Impassable;
+        }
+
+        GameObject element = Instantiate(_prop.physicalObject, referenceSquare.position + _prop.positionOffset, Quaternion.identity);
+        instantiatedLevelPieces.Add(element);
+    }
+
+}
