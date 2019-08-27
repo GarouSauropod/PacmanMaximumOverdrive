68c0a3937526e0f778b68d7a1a86ffa6d10e73d3
GarouSauropod <alexandre@sauropodstudio.com>
2019-08-27 17:27:09 -0400

    Added music

---
+++ Assets/Scripts/Managers/SoundManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SoundManager : MonoBehaviour
+{
+
+    private static SoundManager soundManager;
+
+    public static SoundManager instance
+    {
+        get
+        {
+            if (!soundManager)
+            {
+                soundManager = FindObjectOfType(typeof(SoundManager)) as SoundManager;
+
+                if (!soundManager)
+                {
+                    Debug.LogError("There needs to be one active SoundManager script on a GameObject in your scene.");
+                }
+            }
+
+            return soundManager;
+        }
+    }
+
+    [SerializeField]
+    private AudioClip music;
+    [SerializeField]
+    private AudioSource source;
+
+    void Start()
+    {
+        source.clip = music;
+        source.Play();
+    }
+
+}
